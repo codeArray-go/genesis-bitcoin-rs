dd085fe1bf2b6a521471720460571183c1af294d
codeArray-go <loneranger2520@gmail.com>
2026-06-11 23:17:12 +0530

    updated miner

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,54 +1,78 @@
-use anyhow::{Result, anyhow};
-use btclib::{crypto::PublicKey, network::Message, util::Saveable};
-use std::{env, process::exit};
-use tokio::net::TcpStream;
+use std::{
+    sync::{Arc, atomic::AtomicBool},
+    thread,
+    time::Duration,
+};
 
-fn usage() -> ! {
-    eprintln!(
-        "Usage: {} <address> <public_key_file>",
-        env::args().next().unwrap()
-    );
-    exit(1);
-}
+use anyhow::{Result, anyhow};
+use btclib::{crypto::PublicKey, types::Block, util::Saveable};
+use clap::Parser;
+use tokio::{net::TcpStream, sync::Mutex, time::interval};
 
 #[derive(Parser)]
-#[command(author, version, aboutm long_about = None)]
-pub struct Cli {
+#[command(author, version, about, long_about = None)]
+struct Cli {
     #[arg(short, long)]
-    pub address: String,
+    address: String,
 
     #[arg(short, long)]
-    pub public_key_file: String,
+    public_key_file: String,
+}
+
+struct Miner {
+    public_key: PublicKey,
+    stream: Mutex<TcpStream>,
+    current_templete: Arc<std::sync::Mutex<Option<Block>>>,
+    mining: Arc<AtomicBool>,
+    mined_block_sender: flume::Sender<Block>,
+    mined_block_receiver: flume::Receiver<Block>,
+}
+impl Miner {
+    fn spawn_mining_thread(&self) -> thread::JoinHandle<()> {
+        thread::spawn(|| {})
+    }
+
+    async fn fetch_and_validate_templete(&self) -> Result<()> {}
+
+    async fn submit_block(&self, block: Block) -> Result<()> {}
+
+    async fn run(&self) -> Result<()> {
+        self.spawn_mining_thread();
+        let mut templete_interval = interval(Duration::from_secs(5));
+
+        loop {
+            let receiver_clone = self.mined_block_receiver.clone();
+            tokio::select! {
+                _ = templete_interval.tick() => {
+                    self.fetch_and_validate_templete().await?;
+                }
+                Ok(mined_block) = receiver_clone.recv_async() => {
+                    self.submit_block(mined_block).await?;
+                }
+            }
+        }
+    }
+
+    async fn new(address: String, public_key: PublicKey) -> Result<Self> {
+        let stream = TcpStream::connect(&address).await?;
+        let (mined_block_sender, mined_block_receiver) = flume::unbounded();
+        Ok(Self {
+            public_key,
+            stream: Mutex::new(stream),
+            current_templete: Arc::new(std::sync::Mutex::new(None)),
+            mining: Arc::new(AtomicBool::new(false)),
+            mined_block_sender,
+            mined_block_receiver,
+        })
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    let address = match env::args().nth(1) {
-        Some(address) => address,
-        None => usage(),
-    };
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let public_key = PublicKey::read_from_file(&cli.public_key_file)
+        .map_err(|e| anyhow!("Failed to read public key file {}", e))?;
 
-    let public_key_file = match env::args().nth(2) {
-        Some(public_key_fle) => public_key_fle,
-        None => usage(),
-    };
-
-    let Ok(public_key) = PublicKey::read_from_file(&public_key_file) else {
-        eprint!("Failed to read public key");
-        exit(1);
-    };
-
-    let mut stream = match TcpStream::connect(&address).await {
-        Ok(stream) => stream,
-        Err(e) => {
-            eprintln!("Failed to connect to server: {}", e);
-            exit(1);
-        }
-    };
-
-    println!("Connecting to {address} to mine with {public_key:?}");
-
-    println!("Requesting node for mining.");
-    let message = Message::FetchTemplete(public_key);
-    message.send(&mut stream).await.unwrap();
+    let miner = Miner::new(cli.address, public_key).await?;
+    miner.run().await
 }
